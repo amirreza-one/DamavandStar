61fad0876dd395c50e85d79fbe0813a583070fa8
AmirreZa <amirreza.one@gmail.com>
2014-05-28 19:21:44 +0430

    First Commit
    First Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+DamavandStar::Application.config.secret_key_base = '8fbe44ec14ab4663002cae21c72d3abed10c74b9de3128dcc40a5562c6afd58fa7263463ee3ef4c9afb014b05eb0114f0ad3c1d0878080aed8cd8d45555adae8'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+DamavandStar::Application.config.session_store :cookie_store, key: '_DamavandStar_session'
